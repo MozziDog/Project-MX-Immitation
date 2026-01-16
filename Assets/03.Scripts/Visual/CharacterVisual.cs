6244ce76fa92404d31fb6fd3ec309ee81a8525d7
김태현 <th2252@gmail.com>
2026-01-16 20:57:47 +0900

    Add Shouldering Visualization

--- Assets/03.Scripts/Visual/CharacterVisual.cs
+++ Assets/03.Scripts/Visual/CharacterVisual.cs
@@ -2,109 +2,114 @@ using UnityEngine;
 using Logic;
 // using DamageNumbersPro;
 
-
-// TODO: Replace DamageNumbersPro
-public class CharacterVisual : MonoBehaviour
+namespace Visual
 {
-    public CharacterLogic Logic;
+    // TODO: Replace DamageNumbersPro
+    public class CharacterVisual : MonoBehaviour
+    {
+        public CharacterLogic Logic;
+        [SerializeField] private GameObject gun;
     
-    // [SerializeField] DamageNumber _damageNumberPrefab;
-    Vector3 _positionBeforeFrame;
+        // [SerializeField] DamageNumber _damageNumberPrefab;
+        Vector3 _positionBeforeFrame;
     
-    // Start is called before the first frame update
-    void Start()
-    {
-        _positionBeforeFrame = transform.position;
-        Logic.OnAttack += LookAtEnemy;
-        Logic.OnUseNormalSkill += LookAtEnemy;
-        Logic.OnUseExSkill += LookAtEnemy;
-        Logic.OnReload += OnReloaded;
-        Logic.OnShoulderWeapon += OnShoulder;
-        Logic.OnUnshoulderWeapon += OnUnshoulder;
-        Logic.OnCharacterTakeDamage += DisplayDamageNumber;
-        Logic.OnDie += DestroyVisual;
-    }
+        // Start is called before the first frame update
+        void Start()
+        {
+            _positionBeforeFrame = transform.position;
+            Logic.OnAttack += LookAtEnemy;
+            Logic.OnUseNormalSkill += LookAtEnemy;
+            Logic.OnUseExSkill += LookAtEnemy;
+            Logic.OnReload += OnReloaded;
+            Logic.OnShoulderWeapon += OnShoulder;
+            Logic.OnUnshoulderWeapon += OnUnshoulder;
+            Logic.OnCharacterTakeDamage += DisplayDamageNumber;
+            Logic.OnDie += DestroyVisual;
+        }
 
-    // Update is called once per frame
-    void Update()
-    {
-        var targetPosition = ToVector3(Logic.Position);
+        // Update is called once per frame
+        void Update()
+        {
+            var targetPosition = Util.ToVector3(Logic.Position);
         
-        if (Logic.IsMoving)
-        {
-            Vector3 positionCurrentFrame = targetPosition;
-            // 이동 방향 바라보기
-            transform.LookAt(targetPosition);
-            _positionBeforeFrame = positionCurrentFrame;
-        }
+            if (Logic.IsMoving)
+            {
+                Vector3 positionCurrentFrame = targetPosition;
+                // 이동 방향 바라보기
+                transform.LookAt(targetPosition);
+                _positionBeforeFrame = positionCurrentFrame;
+            }
         
-        // 부드러운 이동을 위한 좌표 보간
-        Vector3 velocity = targetPosition - _positionBeforeFrame;
-        transform.position = Vector3.SmoothDamp(transform.position, targetPosition, ref velocity, 0.02f);
+            // 부드러운 이동을 위한 좌표 보간
+            Vector3 velocity = targetPosition - _positionBeforeFrame;
+            transform.position = Vector3.SmoothDamp(transform.position, targetPosition, ref velocity, 0.02f);
 
-    }
-
-    void LookAtEnemy()
-    {
-        CharacterLogic enemy = Logic.CurrentTarget;
-        if(enemy == null)
-        {
-            return;
         }
-        Position2 targetLogicPosition = enemy.Position;
-        Vector3 targetWorldPosition = new Vector3(targetLogicPosition.x, 0, targetLogicPosition.y);
-        transform.LookAt(targetWorldPosition);
-    }
 
-    void OnReloaded()
-    {
-        // _damageNumberPrefab.Spawn(transform.position, "Reloaded");
-    }
-
-    void OnShoulder()
-    {
-        // _damageNumberPrefab.Spawn(transform.position, "Shouldered Weapon");
-    }
-
-    void OnUnshoulder()
-    {
-        // _damageNumberPrefab.Spawn(transform.position, "Unshouldered Weapon");
-    }
-
-    void DisplayDamageNumber(int damage, bool isCritical, AttackType attackType, ArmorType armorType)
-    {
-        // Debug.Log("데미지 넘버 스폰");
-        // if (_damageNumberPrefab != null)
-        // {
-        //     _damageNumberPrefab.Spawn(transform.position, damage);
-        // }
-    }
-
-    void DestroyVisual()
-    {
-        Destroy(gameObject);
-    }
-
-    private void OnDrawGizmosSelected()
-    {
-        // 사거리 원
-        Gizmos.color = Color.yellow;
-        Gizmos.DrawWireSphere(transform.position, Logic.AttackRange);
-
-        // 이동 목표 위치
-        Gizmos.color = Color.green;
-        Gizmos.DrawSphere(ToVector3(Logic.MoveDest), 0.1f);
-
-        // 공격 대상
-        if (Logic.CurrentTarget != null)
+        void LookAtEnemy()
         {
-            Gizmos.color = Color.red;
-            Gizmos.DrawSphere(new Vector3(Logic.Position.x, 0, Logic.Position.y), 0.1f);
+            CharacterLogic enemy = Logic.CurrentTarget;
+            if(enemy == null)
+            {
+                return;
+            }
+            Position2 targetLogicPosition = enemy.Position;
+            Vector3 targetWorldPosition = new Vector3(targetLogicPosition.x, 0, targetLogicPosition.y);
+            transform.LookAt(targetWorldPosition);
         }
-    }
 
-    private Vector3 ToVector3(Position2 pos)
-    {
-        return new Vector3(pos.x, 0, pos.y);
+        void OnReloaded()
+        {
+            // _damageNumberPrefab.Spawn(transform.position, "Reloaded");
+            // 총구 아래로
+            if(gun)
+                gun.transform.rotation = Quaternion.Euler(60, 0, 0);
+        }
+
+        void OnShoulder()
+        {
+            // _damageNumberPrefab.Spawn(transform.position, "Shouldered Weapon");
+            // 총구 전방으로
+            if(gun)
+                gun.transform.rotation = Quaternion.identity;
+        }
+
+        void OnUnshoulder()
+        {
+            // _damageNumberPrefab.Spawn(transform.position, "Unshouldered Weapon");
+        
+        }
+
+        void DisplayDamageNumber(int damage, bool isCritical, AttackType attackType, ArmorType armorType)
+        {
+            // Debug.Log("데미지 넘버 스폰");
+            // if (_damageNumberPrefab != null)
+            // {
+            //     _damageNumberPrefab.Spawn(transform.position, damage);
+            // }
+        }
+
+        void DestroyVisual()
+        {
+            Destroy(gameObject);
+        }
+
+        private void OnDrawGizmosSelected()
+        {
+            // 사거리 원
+            Gizmos.color = Color.yellow;
+            Gizmos.DrawWireSphere(transform.position, Logic.AttackRange);
+
+            // 이동 목표 위치
+            Gizmos.color = Color.green;
+            Gizmos.DrawSphere(Util.ToVector3(Logic.MoveDest), 0.1f);
+
+            // 공격 대상
+            if (Logic.CurrentTarget != null)
+            {
+                Gizmos.color = Color.red;
+                Gizmos.DrawSphere(new Vector3(Logic.Position.x, 0, Logic.Position.y), 0.1f);
+            }
+        }
     }
 }
